b057ca0630529c6c2eb526a4f08176f813eeee58
Xavier Fischer <xavier.fischer@outlook.com>
2017-06-22 11:44:05 +0200

    Initial commit

---
+++ PlaylistYearizeer/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.btnFindPlaylist = new System.Windows.Forms.Button();
+            this.txtPlaylist = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(188, 51);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Token (get one @ developers.deezer.com/api/explorer)";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(15, 25);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(248, 20);
+            this.txtToken.TabIndex = 0;
+            // 
+            // btnFindPlaylist
+            // 
+            this.btnFindPlaylist.Location = new System.Drawing.Point(179, 49);
+            this.btnFindPlaylist.Name = "btnFindPlaylist";
+            this.btnFindPlaylist.Size = new System.Drawing.Size(75, 23);
+            this.btnFindPlaylist.TabIndex = 3;
+            this.btnFindPlaylist.Text = "Find playlist";
+            this.btnFindPlaylist.UseVisualStyleBackColor = true;
+            this.btnFindPlaylist.Click += new System.EventHandler(this.btnFindPlaylist_Click);
+            // 
+            // txtPlaylist
+            // 
+            this.txtPlaylist.Location = new System.Drawing.Point(6, 23);
+            this.txtPlaylist.Name = "txtPlaylist";
+            this.txtPlaylist.Size = new System.Drawing.Size(248, 20);
+            this.txtPlaylist.TabIndex = 2;
+            this.txtPlaylist.Text = "Loved tracks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Playlist name";
+            // 
+            // btnReport
+            // 
+            this.btnReport.Location = new System.Drawing.Point(6, 85);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(142, 23);
+            this.btnReport.TabIndex = 4;
+            this.btnReport.Text = "Create playlists by year";
+            this.btnReport.UseVisualStyleBackColor = true;
+            this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
+            // 
+            // txtReport
+            // 
+            this.txtReport.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReport.Location = new System.Drawing.Point(6, 114);
+            this.txtReport.Multiline = true;
+            this.txtReport.Name = "txtReport";
+            this.txtReport.ReadOnly = true;
+            this.txtReport.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReport.Size = new System.Drawing.Size(335, 119);
+            this.txtReport.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtReport);
+            this.panel1.Controls.Add(this.btnFindPlaylist);
+            this.panel1.Controls.Add(this.btnReport);
+            this.panel1.Controls.Add(this.txtPlaylist);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(15, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 236);
+            this.panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 337);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Button btnFindPlaylist;
+        private System.Windows.Forms.TextBox txtPlaylist;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.TextBox txtReport;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ PlaylistYearizeer/Form1.cs
@@ -0,0 +1,163 @@
+﻿using E.Deezer;
+using E.Deezer.Api;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Deezer _deezer;
+        IPlaylist _playlist;
+        List<IPlaylist> _playlists;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormClosing += (o, e) => { if (_deezer != null) _deezer.Logout(); };
+        }
+
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _deezer = DeezerSession.CreateNew();
+
+                await _deezer.Login(txtToken.Text);
+
+                AddToLog("Logged in.");
+
+                panel1.Enabled = true;
+            }
+            catch (Exception v_ex)
+            {
+                MessageBox.Show(v_ex.ToString());
+            }
+        }
+
+        private async void btnFindPlaylist_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _playlists = (await _deezer.User.Current.GetPlaylists(250)).ToList();
+                var test = _playlists.FirstOrDefault(p => p.IsLovedTrack);
+
+                _playlist = _playlists.FirstOrDefault(p => p.Title.ToLower() == txtPlaylist.Text.ToLower());
+
+                if (_playlist == null)
+                {
+                    AddToLog("Playlist not found !");
+                }
+                else
+                {
+                    AddToLog("Playlist found.");
+                    txtPlaylist.Text = _playlist.Title;
+                }
+
+
+            }
+            catch (Exception v_ex)
+            {
+                MessageBox.Show(v_ex.ToString());
+            }
+
+
+        }
+
+        private void AddToLog(string logString)
+        {
+            txtReport.AppendText(logString);
+            txtReport.AppendText(Environment.NewLine);
+        }
+
+        private async void btnReport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var tracks = await _playlist.GetTracks(_playlist.NumTracks);
+
+
+                var tracksByYear = from track in tracks
+                                   group track by track.TimeAdd.Year into g
+                                   select new PlaylistReport
+                                   {
+                                       Year = g.Key // year
+                                               ,
+                                       PlaylistTitleWithYear = _playlist.Title.TrimEnd() + " " + g.Key.ToString() // playlist + year
+                                               ,
+                                       PlaylistId = 0
+                                               ,
+                                       TrackIds = g.Select(t => t.Id).ToList()
+                                   }; // list of track ids for easier add
+                List<PlaylistReport> playlistsReport = tracksByYear.OrderBy(r => r.Year).ToList();
+
+                string report = null;
+                foreach (var groups in playlistsReport)
+                {
+                    report = string.Concat(report, groups.Year, ": ", groups.TrackIds.Count, " song(s).", Environment.NewLine);
+                }
+                AddToLog(report);
+
+                // Get or create playlist
+                foreach (PlaylistReport playlistReport in playlistsReport)
+                {
+                    IPlaylist playlist = _playlists.FirstOrDefault(p => p.Title == playlistReport.PlaylistTitleWithYear);
+                    if (playlist != null)
+                    {
+                        AddToLog("Playlist <" + playlistReport.PlaylistTitleWithYear + "> exists");
+                        playlistReport.PlaylistId = playlist.Id;
+                    }
+                    else
+                    {
+                        // create playlist
+                        AddToLog("Create playlist <" + playlistReport.PlaylistTitleWithYear + ">");
+                        uint id = await _deezer.User.Current.CreatePlaylist(playlistReport.PlaylistTitleWithYear);
+                        playlistReport.PlaylistId = id;
+                    }
+                }
+
+                // Add songs to playlist by year
+                foreach (PlaylistReport playlistReport in playlistsReport)
+                {
+                    AddToLog("Adding " + playlistReport.TrackIds.Count.ToString() + " song(s) to playlist <" + playlistReport.PlaylistTitleWithYear + ">");
+                    int skip = 0;
+                    var nextSongIds = playlistReport.TrackIds.Skip(skip).Take(10);
+                    while (nextSongIds.Any())
+                    {
+                        string songIdsQuery = string.Join<long>(",", nextSongIds);
+
+                        await _deezer.User.Current.AddToPlaylist(playlistReport.PlaylistId, songIdsQuery);
+                        skip += 10;
+                        nextSongIds = playlistReport.TrackIds.Skip(skip).Take(10);
+                    }
+                }
+
+                AddToLog("Done !");
+
+
+            }
+            catch (Exception v_ex)
+            {
+                MessageBox.Show(v_ex.ToString());
+            }
+        }
+    }
+
+    internal class PlaylistReport
+    {
+        public uint PlaylistId { get; set; }
+        public string PlaylistTitleWithYear { get; set; }
+        public List<long> TrackIds { get; set; }
+        public int Year { get; set; }
+    }
+}
